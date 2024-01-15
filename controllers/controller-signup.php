02021eecddb6c7b5d5ed2f10347d8785db113b83
Shamsddine <goumdissshamsddine@gmail.com>
2024-01-15 16:00:33 +0100

    first commit

---
+++ controllers/controller-signup.php
@@ -0,0 +1,133 @@
+<?php
+// Vérification des données postées depuis le formulaire
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $errors = [];
+
+    // Vérification du nom
+    if (empty($_POST["nom"])) {
+        $errors['firstname'] = "Champs obligatoire.";
+    } else if (!preg_match("/^[a-zA-ZÀ-ÿ\-]+$/", $_POST["firstname"])) {
+        $errors['firstname'] = "Le nom est invalide.";
+    }
+
+    // Vérification du prénom
+    if (empty($_POST["firstname"])) {
+        $errors['firstname'] = "Champs obligatoire.";
+    } else if (!preg_match("/^[a-zA-ZÀ-ÿ\-]+$/", $_POST["lastname"])) {
+        $errors['firstname'] = "Le nom est invalide.";
+    }
+
+    // Verification du select
+    // Vérifie si la variable POST 'id_entreprise' est définie et n'est pas vide
+if (isset($_POST['id_$enterprise']) && !empty($_POST['id_$enterprise'])) {
+    $id_enterprise = $_POST['id_$enterprise'];
+
+    // Vérifie si la valeur est l'une des valeurs attendues
+    if ($id_enterprise === "$enterpriseA" || $id_enterprise === "$enterpriseB") {
+        // La valeur est valide, vous pouvez faire ce que vous avez besoin de faire avec $id_$enterprise
+        echo "ID de l'$enterprise valide : " . $id_enterprise;
+    } else {
+        // La valeur n'est pas valide
+        echo "Valeur de l'ID de l'$enterprise non valide.";
+    }
+} else {
+    // La variable n'a pas été définie ou est vide
+    echo "L'ID de l'enterprise n'a pas été fourni ou est vide.";
+}
+
+    // Vérification du Pseudo
+    $regexPseudo = '/^[a-zA-Z_\-\d]+$/';
+    if (empty($_POST["pseudo"])) {
+        $errors['pseudo'] = "Champs obligatoire.";
+    } 
+
+    // Vérification de l'email
+    if (empty($_POST["email"])) {
+        $errors['email'] = "Champs obligatoire.";
+    } else if (!filter_var($_POST["email"], FILTER_VALIDATE_EMAIL)) {
+        $errors['email'] = "L'adresse email est invalide.";
+    }
+
+    // Vérification de la date de naissance
+    if (empty($_POST["dob"])) {
+        $errors['dob'] = "La date de naissance est obligatoire.";
+    }
+
+    // Vérification du mot de passe
+    $password = $_POST["password"];
+    $confirm_password = $_POST["confirm_password"];
+    if (empty($password) || strlen($password) < 8 || $password !== $confirm_password) {
+        $errors['password'] = "Le mot de passe doit comporter au moins 8 caractères et correspondre.";
+    }
+
+    if (!isset($_POST["CGU"])) {
+        $errors['CGU'] = "Les CGU sont obligatoires";
+    }
+
+    // Affichage des erreurs
+    // Si aucune erreur détectée
+    if (empty($errors)) {
+        // Afficher la synthèse des informations et le message de confirmation
+        $nom = $_POST['nom'];
+        $prenom = $_POST['prenom'];
+        $email = $_POST['email'];
+        $dob = $_POST['dob'];
+
+        // Cacher le formulaire
+        $formHidden = true;
+
+        // Envoyer le mail de confirmation (simulation ici)
+        $message = "Bonjour $prenom $nom, votre inscription a bien été enregistrée.";
+        // Envoi du mail (à remplacer par votre code d'envoi de mail)
+        // mail($email, 'Confirmation d\'inscription', $message);
+
+        // Afficher la synthèse des informations et le message de confirmation
+        if(empty($errors)){
+            // Instance d'une PDO
+            try {
+                $dbName = "geralt";
+                $dbUser = "geralt";
+                $dbPassword ="amine123";
+               $db = new PDO("mysql:host=localhost;dbname=$dbName", $dbUser,  $dbPassword);
+               var_dump($db);
+               //stockage de ma requete toujours avant le catch
+               $sql = "INSERT INTO `userprofil`(`user_validate`, `user_name`, `user_firstname`, `user_pseudo`, `user_describ`, `user_email`, `user_dateofbirth`, `user_password`, `user_photo`, `enterprise_id`) VALUES (:lastname, : firstname, :pseudo, :birthdate, :email, :password, :id_enterprise, : valide_participant)";
+        
+               $query = $db->prepare($sql);
+        
+               $lastname = htmlspecialchars($_POST['lastname']);
+               $firstname = htmlspecialchars($_POST['firstname']);
+               $pseudo = htmlspecialchars($_POST['pseudo']);
+               $dob = $_POST['dob'];
+               $email = $_POST['email'];
+               $password = password_hash($_POST['password'], PASSWORD_DEFAULT);
+               $enterprise =$_POST['id_enterprise'];
+        
+               $query->bindValue(':lastname', $lastname, PDO::PARAM_STR);
+               $query->bindValue(':firstname', $firstname, PDO::PARAM_STR);
+               $query->bindValue(':pseudo', $pseudo, PDO::PARAM_STR);
+               $query->bindValue(':birthdate', $dob, PDO::PARAM_INT);
+               $query->bindValue(':email', $email, PDO::PARAM_STR);
+               $query->bindValue(':password', $password, PDO::PARAM_STR);
+               $query->bindValue(':id_entreprise', $enterprise, PDO::PARAM_STR);
+               $query->bindValue(':valide_participant', 1, PDO::PARAM_STR);
+        
+            } catch (PDOException $e){
+                echo "Erreur : " . $e->getMessage();
+                die();
+            }
+        include('../views/view-summary.php');
+        exit; // Arrêter l'exécution du script
+    }
+}
+?>
+<?php
+
+}
+// Contrôleur - Gestion de la logique métier
+
+// Vérifications et traitements du formulaire ici
+
+// Inclusion de la vue
+include_once('../views/view-signup.php');
+?>
---
+++ views/view-signup.php
@@ -0,0 +1,121 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    
+    <!-- Header -->
+    <header>
+        <!-- Votre code pour l'en-tête -->
+    </header>
+
+    <h2>Inscription</h2>
+
+    <form action="controller-signup.php" method="POST" novalidate>
+        <label for="firstname">firstname:</label><br>
+        <input type="text" id="firstname" name="firstname"
+            value="<?= isset($_POST['firstname']) ? htmlspecialchars($_POST['firstname']) : '' ?>">
+        <span class="error">
+            <?php if (isset($errors['firstname'])) {
+                echo $errors['firstname'];
+            } ?>
+        </span><br><br>
+
+        <label for="lastname">Prénom:</label><br>
+        <input type="text" id="lastname" name="lastname"
+            value="<?= isset($_POST['lastname']) ? htmlspecialchars($_POST['lastname']) : '' ?>">
+        <span class="error">
+            <?php if (isset($errors['lastname'])) {
+                echo $errors['lastname'];
+            } ?>
+        </span><br><br>
+
+        <label for="pseudo">Pseudo:</label><br>
+        <input type="text" id="pseudo" name="pseudo" 
+            value="<?= isset($_POST['pseudo']) ? htmlspecialchars($_POST['pseudo']) : '' ?>">
+        <span class="error">
+            <?php if (isset($errors['pseudo'])) {
+                echo $errors['pseudo'];
+            } ?>
+        </span><br><br>
+
+        <label for="email">Courriel:</label><br>
+        <input type="email" id="email" name="email"
+            value="<?= isset($_POST['email']) ? htmlspecialchars($_POST['email']) : '' ?>">
+        <span class="error">
+            <?php if (isset($errors['email'])) {
+                echo $errors['email'];
+            } ?>
+        </span><br><br>
+
+        <div>
+        <label for="enterprise">enterprise:</label><br>
+        <select type="text" id="enterprise" name="enterprise"
+            value="<?= isset($_POST['enterprise']) ? htmlspecialchars($_POST['enterprise']) : '' ?>">
+            <option value="">--please choose an option---</option>
+            <option value="enterpriseA">enterprise A</option>
+            <option value="enterpriseB">enterprise B</option>
+        <span class="error">
+            <?php if (isset($errors['enterprise'])) {
+                echo $errors['enterprise'];
+            } ?> 
+            </span>
+            </select>
+        </div>
+
+        <div>
+        <label for="dob">Date de naissance:</label><br>
+        <input type="date" id="dob" name="dob"
+            value="<?= isset($_POST['dob']) ? htmlspecialchars($_POST['dob']) : '' ?>">
+        <span class="error">
+            <?php if (isset($errors['dob'])) {
+                echo $errors['dob'];
+            } ?>
+        </span><br>
+        </div>
+
+        <label for="password">Mot de passe:</label><br>
+        <input type="password" id="password" name="password"
+            value="<?= isset($_POST['passsword']) ? htmlspecialchars($_POST['password']) : '' ?>">
+        <span class="error">
+            <?php if (isset($errors['password'])) {
+                echo $errors['password'];
+            } ?>
+        </span><br><br>
+
+        <label for="confirm_password">Confirmer le mot de passe:</label><br>
+        <input type="password" id="confirm_password" name="confirm_password"
+            value="<?= isset($_POST['confirm_password']) ? htmlspecialchars($_POST['confirm_password']) : '' ?>">
+        <span class="error">
+            <?php if (isset($errors['confirm_password'])) {
+                echo $errors['confirm_password'];
+            } ?>
+        </span><br><br>
+
+        <label for="CGU"></label><br>
+        <input type="checkbox" id="CGU" name="CGU"
+            <?= isset($_POST['CGU']) ? "checked"  : '' ?>> Veuillez accepter les CGU
+        <span class="error">
+            <?php if (isset($errors['CGU'])) {
+                echo $errors['CGU'];
+            } ?>
+        </span><br><br>
+
+        <input type="submit" value="S'enregistrer">
+    </form>
+
+    <!-- Footer -->
+    <footer>
+        <!-- Votre code pour le pied de page -->
+    </footer>
+
+
+
+</body>
+
+</html>

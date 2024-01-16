b3769456b41aea8ebff9914fc2fc5f8cd2d3d198
Shamsddine <goumdissshamsddine@gmail.com>
2024-01-16 09:21:14 +0100

    amine tais toi

---
+++ config/config.php
@@ -0,0 +1,4 @@
+define('DB_HOST', 'localhost');
+define('DB_USER', 'geralt');
+define('DB_PASS', 'am12ine34');
+define('DB_NAME', 'geralt');
--- models/utilisateur.php
+++ models/utilisateur.php
@@ -1,40 +1,33 @@
 <?php  
 
 class user {
-    public function create () {
-        try {
-            $dbName = "geralt";
-            $dbUser = "geralt";
-            $dbPassword ="am12ine34";
-            $db = new PDO("mysql:host=localhost;dbname=$dbName", $dbUser,  $dbPassword);
-            var_dump($db);
-            //stockage de ma requete toujours avant le catch
-            $sql = "INSERT INTO `userprofil`(`user_validate`, `user_name`, `user_firstname`, `user_pseudo`, `user_email`, `user_dateofbirth`, `user_password`, `enterprise_id`) VALUES (:userValidate, :lastname, :firstname, :pseudo, :email, :birthdate, :userPassword, :id_enterprise)";
-    
-            $query = $db->prepare($sql);
-    
-            $lastname = htmlspecialchars($_POST['lastname']);
-            $firstname = htmlspecialchars($_POST['firstname']);
-            $pseudo = htmlspecialchars($_POST['pseudo']);
-            $email = $_POST['email'];
-            $dob = $_POST['dob'];
-            $password = password_hash($_POST['password'], PASSWORD_DEFAULT);
-            $id_enterprise =$_POST['id_enterprise'];
-        
-            $query->bindValue(':userValidate',1, PDO::PARAM_INT);
-            $query->bindValue(':lastname', $lastname, PDO::PARAM_STR);
-            $query->bindValue(':firstname', $firstname, PDO::PARAM_STR);
-            $query->bindValue(':pseudo', $pseudo, PDO::PARAM_STR);
-            $query->bindValue(':email', $email, PDO::PARAM_STR);
-            $query->bindValue(':birthdate', $dob, PDO::PARAM_STR);
-            $query->bindValue(':userPassword', $password, PDO::PARAM_STR);
-            $query->bindValue(':id_enterprise', $id_enterprise, PDO::PARAM_STR);
+public function create ( $userValidate, $lastname, $firstname, $pseudo, $password, $email, $dob, $password, $id_enterprise) {
+    try {
+        $dbName = "geralt";
+        $dbUser = "geralt";
+        $dbPassword ="am12ine34";
+        $db = new PDO("mysql:host=localhost;dbname=$dbName", $dbUser,  $dbPassword);
+        var_dump($db);
+        //stockage de ma requete toujours avant le catch
+        $sql = "INSERT INTO `userprofil`(`user_validate`, `user_name`, `user_firstname`, `user_pseudo`, `user_email`, `user_dateofbirth`, `user_password`, `enterprise_id`) VALUES (:userValidate, :lastname, :firstname, :pseudo, :email, :birthdate, :userPassword, :id_enterprise)";
 
-            $query->execute();
-        } catch (PDOException $e){
-            echo "Erreur : " . $e->getMessage();
-            die();
-        }  
+        $query = $db->prepare($sql);
+        
+    
+        $query->bindValue(':userValidate',1, PDO::PARAM_INT);
+        $query->bindValue(':lastname', $lastname, PDO::PARAM_STR);
+        $query->bindValue(':firstname', $firstname, PDO::PARAM_STR);
+        $query->bindValue(':pseudo', $pseudo, PDO::PARAM_STR);
+        $query->bindValue(':email', $email, PDO::PARAM_STR);
+        $query->bindValue(':birthdate', $dob, PDO::PARAM_STR);
+        $query->bindValue(':userPassword', $password, PDO::PARAM_STR);
+        $query->bindValue(':id_enterprise', $id_enterprise, PDO::PARAM_STR);
+
+        $query->execute();
+    } catch (PDOException $e){
+        echo "Erreur : " . $e->getMessage();
+        die();
+    }  
 }
 
 ?>

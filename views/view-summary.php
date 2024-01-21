8ab9eed52457e7f9f40a56bd3145b0634db98fef
zirael <goumdissshamsddine@gmailcom>
2024-01-21 21:35:52 +0100

    poto

--- views/view-summary.php
+++ views/view-summary.php
@@ -5,6 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Synthèse d'inscription</title>
+    <link rel="stylesheet" href="../assets/style/summary.css">
 </head>
 
 <body>
---
+++ views/views-home.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../assets/style/home.css">
+    <title>Home</title>
+</head>
+<body>
+    
+<h1>Bienvenue
+        <?php echo $pseudo; ?>.
+</h1>
+
+<div class="button">
+    <button>Créer un trajet</button>
+    <button>Modifier un trajet</button>
+</div>
+
+
+</body>
+</html>
